d49aa1d0bece213bfdf9121ccd2dd00e51698928
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-08-21 10:15:59 +0000

    Deletion of All files of the project due to some Technical Issue

--- Movix-App/src/Components/contentWrapper/ContentWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./ContentWrapper.scss";
-
-const ContentWrapper = ({ children }) => {
-  return <div className="contentWrapper">{children}</div>;
-};
-
-export default ContentWrapper;
